2d120c9a4e9ca07e7fddff5c398fc9113a2118b0
Meredith Grubbs <meregrubbs@gmail.com>
2021-08-11 10:56:12 -0600

    testimonial data inside ClientTestimonials

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Hero from './home_components/Hero';
 import Portfolio from './home_components/Portfolio';
-import Testimonial from './home_components/Testimonial';
+import ClientTestimonials from './home_components/ClientTestimonials';
 import Footer from './header_footer/Footer';
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
     <>
       <Hero />
       <Portfolio />
-      <Testimonial />
+      <ClientTestimonials />
       <Footer />
     </>
   );
---
+++ src/pages/home_components/ClientTestimonials.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import aliceImage from '../../images/testimonial-images/alice.png';
+import maressaImage from '../../images/testimonial-images/maressa.png';
+import emmieImage from '../../images/testimonial-images/emmie.png';
+
+// markup
+const ClientTestimonials = () => {
+  // data
+  const testimonials = [
+    {
+      name: 'Alice Dubin',
+      title: 'Journalist & Content Marketing Writer',
+      text:
+        '[Meredith’s] work is just top quality... I feel that my website represents exactly what I hoped to achieve... I am really thrilled with the result (and so relieved I didn’t try to do this myself without her expertise).',
+      img: aliceImage
+    },
+    {
+      name: 'Maressa Brown',
+      title: 'Journalist & Content Strategist',
+      text:
+        'Meredith Grubbs is an intuitive, creative, and communicative designer who is perpetually taking her skills to the next level... [She] worked diligently to ensure that my site fully represented my brand.',
+      img: maressaImage
+    },
+    {
+      name: 'Emaline Brown',
+      title: 'Naturopathic Physician',
+      text:
+        'It was a pleasure working with Meredith in designing my first website! She is patient and detail-oriented... She even took the time to meet and show me the basics on Wordpress... So grateful for my beautiful site, I would choose her again in an instant!',
+      img: emmieImage
+    }
+  ];
+
+  return (
+    <section className='testimonials'>
+      <h1 className='section-title'>Testimonials</h1>
+      {testimonials.map(testimonial => (
+        <div className='single-testimonial' key={testimonial.name}>
+          <div className='testimonial-img'>
+            <img src={testimonial.img} alt={testimonial.name} />
+          </div>
+
+          <div className='testimonial-text'>
+            <p className='text'>{testimonial.text}</p>
+            <p className='name'>{testimonial.name}</p>
+            <p className='title'>{testimonial.title}</p>
+          </div>
+        </div>
+      ))}
+      <Link className='button hero-button' to='/contact'>
+        Let's Work Together
+      </Link>
+    </section>
+  );
+};
+
+export default ClientTestimonials;
