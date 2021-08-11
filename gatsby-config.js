77b8780d940e7b08342aff63853c2f9b3f74da0e
Meredith Grubbs <meregrubbs@gmail.com>
2021-08-11 10:16:36 -0600

    gatsby-plugin-manifest, gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,27 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "MEG Portfolio 2021",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Meredith Grubbs | Web Designer & Developer',
+    description:
+      'Meredith Grubbs is a freelance web designer and developer based in Durango, CO. She specializes in designing websites for small businesses.'
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
+    'gatsby-plugin-netlify',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-179308532-1",
-      },
+        trackingId: 'UA-179308532-1'
+      }
     },
-  ],
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        icon: 'src/images/meg-logo-1.png'
+      }
+    }
+  ]
 };
