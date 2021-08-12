5d106ec72dae3b4dd1bf805c542f266973566940
Meredith Grubbs <meregrubbs@gmail.com>
2021-08-12 11:43:16 -0600

    data inside portfolio-alice/maressa markup

--- src/pages/Portfolio-Alice.js
+++ src/pages/Portfolio-Alice.js
@@ -7,83 +7,83 @@ import casestudyJournalism from '../images/case-study-images/alice/alice-journal
 import casestudyContentMarketing from '../images/case-study-images/alice/alice-content-marketing.png';
 import casestudyClientCollage from '../images/case-study-images/alice/alice-client-collage.png';
 
-// data
-const alicePortfolioIntroData = {
-  title: 'Alice Dubin Media',
-  subtitle: 'An online portfolio showcasing professional journalism work.',
-  services: [
-    'UX/UI design',
-    'Front-end development, HTML/CSS/JS',
-    'Custom Wordpress theme',
-    'Hosting & Domain setup'
-  ],
-  website: 'www.alicedubin.com',
-  img: alicePortfolioImg
-};
-
-const aliceCaseStudy = [
-  {
-    heading: 'The Project',
-    paragraphs: [
-      'Alice Dubin is a professional journalist and content marketing writer. She’s written for major publications such as Bravo TV, Business Insider, Good Housekeeping, Parents Magazine and many more.',
-
-      'As a freelance journalist, much of her work was scattered around across many different websites. She needed a good website to showcase all of her work in one place.'
-    ],
-    images: []
-  },
-  {
-    heading: 'Organization is Key',
-    paragraphs: [
-      'As Alice had work in both the journalism space as well as the content marketing space, it was important to come up with a solutions for users to easily flip through her work. '
-    ],
-    images: [
-      {
-        pic: casestudyMainPortfolioPageImg,
-        caption: 'Main Portfolio Page'
-      }
-    ]
-  },
-  {
-    heading: 'Viewable by Category',
-    paragraphs: [
-      'In addition to work being divided into journalism verses content marketing, she also needed her articles to be divided according to the topic.',
-
-      'A prospective client may just be interested in a specific subject matter, or they might be interested in viewing all of Alice’s work. We needed to make it easy for whichever type of user may be visiting her website.'
-    ],
-    images: [
-      {
-        pic: casestudyJournalism,
-        caption: 'Journalism Page'
-      },
-      {
-        pic: casestudyContentMarketing,
-        caption: 'Content Marketing Page'
-      }
-    ]
-  },
-  {
-    heading: 'Brand Recognition',
-    paragraphs: [
-      'One of the most important things to Alice’s prospective clients is brand recognition of her past work. If she could easily show the companies she’s worked with in the past, that would give her instant credibility.'
-    ],
-    images: [
-      {
-        pic: casestudyClientCollage,
-        caption: 'Client Collage on Home Page'
-      }
-    ]
-  },
-  {
-    heading: 'Results',
-    paragraphs: [
-      'The website was a success, and I’ve continued to work with Alice on new features for her website. She reported a significant increase in new business after the launch of her website.'
-    ],
-    images: []
-  }
-];
-
 // markup
 const PortfolioAlice = () => {
+  // data
+  const alicePortfolioIntroData = {
+    title: 'Alice Dubin Media',
+    subtitle: 'An online portfolio showcasing professional journalism work.',
+    services: [
+      'UX/UI design',
+      'Front-end development, HTML/CSS/JS',
+      'Custom Wordpress theme',
+      'Hosting & Domain setup'
+    ],
+    website: 'www.alicedubin.com',
+    img: alicePortfolioImg
+  };
+
+  const aliceCaseStudy = [
+    {
+      heading: 'The Project',
+      paragraphs: [
+        'Alice Dubin is a professional journalist and content marketing writer. She’s written for major publications such as Bravo TV, Business Insider, Good Housekeeping, Parents Magazine and many more.',
+
+        'As a freelance journalist, much of her work was scattered around across many different websites. She needed a good website to showcase all of her work in one place.'
+      ],
+      images: []
+    },
+    {
+      heading: 'Organization is Key',
+      paragraphs: [
+        'As Alice had work in both the journalism space as well as the content marketing space, it was important to come up with a solutions for users to easily flip through her work. '
+      ],
+      images: [
+        {
+          pic: casestudyMainPortfolioPageImg,
+          caption: 'Main Portfolio Page'
+        }
+      ]
+    },
+    {
+      heading: 'Viewable by Category',
+      paragraphs: [
+        'In addition to work being divided into journalism verses content marketing, she also needed her articles to be divided according to the topic.',
+
+        'A prospective client may just be interested in a specific subject matter, or they might be interested in viewing all of Alice’s work. We needed to make it easy for whichever type of user may be visiting her website.'
+      ],
+      images: [
+        {
+          pic: casestudyJournalism,
+          caption: 'Journalism Page'
+        },
+        {
+          pic: casestudyContentMarketing,
+          caption: 'Content Marketing Page'
+        }
+      ]
+    },
+    {
+      heading: 'Brand Recognition',
+      paragraphs: [
+        'One of the most important things to Alice’s prospective clients is brand recognition of her past work. If she could easily show the companies she’s worked with in the past, that would give her instant credibility.'
+      ],
+      images: [
+        {
+          pic: casestudyClientCollage,
+          caption: 'Client Collage on Home Page'
+        }
+      ]
+    },
+    {
+      heading: 'Results',
+      paragraphs: [
+        'The website was a success, and I’ve continued to work with Alice on new features for her website. She reported a significant increase in new business after the launch of her website.'
+      ],
+      images: []
+    }
+  ];
+
   return (
     <section className='portfolio-page'>
       <PortfolioTemplate
--- src/pages/Portfolio-Maressa.js
+++ src/pages/Portfolio-Maressa.js
@@ -6,68 +6,68 @@ import maressaFeatured from '../images/case-study-images/maressa/maressa-feature
 import maressaPortfolio from '../images/case-study-images/maressa/maressa-portfolio-images.png';
 import maressaSparkle from '../images/case-study-images/maressa/maressa-sparkle.png';
 
-// data
-const maressaPortfolioIntroData = {
-  title: 'Maressa Brown',
-  subtitle:
-    'Professional website for freelance journalist, editor and astrologer.',
-  services: [
-    'UX/UI design',
-    'Front-end development, HTML/CSS/JS',
-    'Custom Wordpress theme',
-    'Hosting & Domain setup'
-  ],
-  website: 'www.maressabrown.com',
-  img: maressaPortfolioImg
-};
-
-const maressaCaseStudy = [
-  {
-    heading: 'The Project',
-    paragraphs: [
-      'Maressa is a freelance lifestyle journalist, content strategist, author, editor and astrologer. The website she was using to showcase her work was very outdated and difficult to use. She needed a website to display her professional work and give a sense of her personality.'
-    ],
-    images: []
-  },
-  {
-    heading: 'Up-to-date Articles',
-    paragraphs: [
-      'Journalists need to show off their most recent and relevant work, and Maressa is no different. It was important for her to display various featured articles right at the top of her website where users could easily find them.'
-    ],
-    images: [
-      {
-        pic: maressaFeatured,
-        caption: 'Featured Stories on Home Page'
-      }
-    ]
-  },
-  {
-    heading: 'Personality First',
-    paragraphs: [
-      'Maressa is an incredibly fun person to work with! That needed to be made clear on her website. I included many fun details such as interesting photos and bright colors throughout the site.'
-    ],
-    images: [
-      {
-        pic: maressaSparkle,
-        caption: 'Pops of sparkle — logo & background image'
-      },
-      {
-        pic: maressaPortfolio,
-        caption: 'Bright, fun images'
-      }
-    ]
-  },
-  {
-    heading: 'Results',
-    paragraphs: [
-      'Maressa loves her new site, and I’ve continued to work with her to add new features. She reported an increase in website traffic after the launch of her site, and plans to use this website to promote her new upcoming book.'
-    ],
-    images: []
-  }
-];
-
 // markup
 const PortfolioMaressa = () => {
+  // data
+  const maressaPortfolioIntroData = {
+    title: 'Maressa Brown',
+    subtitle:
+      'Professional website for freelance journalist, editor and astrologer.',
+    services: [
+      'UX/UI design',
+      'Front-end development, HTML/CSS/JS',
+      'Custom Wordpress theme',
+      'Hosting & Domain setup'
+    ],
+    website: 'www.maressabrown.com',
+    img: maressaPortfolioImg
+  };
+
+  const maressaCaseStudy = [
+    {
+      heading: 'The Project',
+      paragraphs: [
+        'Maressa is a freelance lifestyle journalist, content strategist, author, editor and astrologer. The website she was using to showcase her work was very outdated and difficult to use. She needed a website to display her professional work and give a sense of her personality.'
+      ],
+      images: []
+    },
+    {
+      heading: 'Up-to-date Articles',
+      paragraphs: [
+        'Journalists need to show off their most recent and relevant work, and Maressa is no different. It was important for her to display various featured articles right at the top of her website where users could easily find them.'
+      ],
+      images: [
+        {
+          pic: maressaFeatured,
+          caption: 'Featured Stories on Home Page'
+        }
+      ]
+    },
+    {
+      heading: 'Personality First',
+      paragraphs: [
+        'Maressa is an incredibly fun person to work with! That needed to be made clear on her website. I included many fun details such as interesting photos and bright colors throughout the site.'
+      ],
+      images: [
+        {
+          pic: maressaSparkle,
+          caption: 'Pops of sparkle — logo & background image'
+        },
+        {
+          pic: maressaPortfolio,
+          caption: 'Bright, fun images'
+        }
+      ]
+    },
+    {
+      heading: 'Results',
+      paragraphs: [
+        'Maressa loves her new site, and I’ve continued to work with her to add new features. She reported an increase in website traffic after the launch of her site, and plans to use this website to promote her new upcoming book.'
+      ],
+      images: []
+    }
+  ];
+
   return (
     <section className='portfolio-page'>
       <PortfolioTemplate
